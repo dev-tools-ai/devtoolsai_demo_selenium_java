d8a2675baa0098ddd1eecb1dfd9b034b11d8ac0d
Etienne DEGUINE <etienne@dev-tools.ai>
2022-09-21 17:01:53 -0700

    i

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# devtoolsai_demo_selenium_java
-A sample IntelliJ project showing how to use the SmartDriver with Java
+# devtoolsai_demo_selenium_python
+Sample project showing how to use SmartDriver with Selenium python.
+The project is meant to be open with the IDE IntelliJ from JetBrains. If you use Eclipse or others you will have to adjust the instructions, particularly the env variable part.
+
+
+# Setup
+Open the project with IntelliJ
+
+Trigger a gradle sync to install all the dependencies
+![Screenshot of Gradle window with mouse on the sync button](gradle_sync.png)
+
+
+# Run the tests
+
+There are two tests:
+ - DemoTest:showFindByAI that shows how to build a test case without locator by prompting the user for bounding boxes
+ - DemoTEst:showIngest that shows how an existing test case using xpaths or other locators can be ingested to become visual elements.
+
+
+## Find by AI test
+
+To fully experience this one, you need to enable an environment variable (DEVTOOLSAI_INTERACTIVE=1)
+To do that, right click on the showFindByAI function name and click 'Modify run configuration', then input `DEVTOOLSAI_INTERACTIVE=1`
+![Screenshot of env variables window](env_variable.png)
+
+After that you can right click and click on 'Run showFindByAI', the test will run and you will be prompted to draw the search box.
+
+## Ingest test
+You can right click and click on 'Run showIngest', the test will run and the elements will be ingested in the dashboard
---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,55 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+// Import DevTools
+import ai.devtools.selenium.SmartDriver;
+
+public class DemoTest
+{
+    @Test
+    public void showIngest() throws Exception {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver chromeDriver = new ChromeDriver();
+        try
+        {
+            SmartDriver driver = new SmartDriver(chromeDriver, "123456");
+            driver.get("https://github.com/login");
+
+            Thread.sleep(1000);
+
+            WebElement loginField = driver.findElement(By.xpath("//input[@id=\"login_field\"]"));
+            loginField.sendKeys("my_username");
+
+            Thread.sleep(1000);
+        }
+        finally
+        {
+            chromeDriver.quit();
+        }
+    }
+
+    @Test
+    public void showFindByAI() throws Exception {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver chromeDriver = new ChromeDriver();
+        try
+        {
+            SmartDriver driver = new SmartDriver(chromeDriver, "123456");
+            driver.get("https://duckduckgo.com");
+
+            Thread.sleep(1000);
+
+            WebElement loginField = driver.findByAI("java_searchbox");
+            loginField.sendKeys("Hello World!");
+
+            Thread.sleep(1000);
+        }
+        finally
+        {
+            chromeDriver.quit();
+        }
+    }
+}
